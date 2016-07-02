a4fee328838c5664c2ba22fe2dc0efb31780fc24
Becca <rebeccamaccio@gmail.com>
2016-07-02 18:51:15 +0200

    mise en page + scrore plus negatif

--- script.js
+++ script.js
@@ -12,21 +12,20 @@ function clicker() {
 //fontion ajout de point + le multiplicateur
 function clicker() {
     score = score + multi
-    document.getElementById("score").innerHTML = score;
-} if (score>=10) {
-    score = setInterval(onclick, 1000);
-}
+    document.getElementById("score").innerHTML = "Points " + score;
+} 
 
 
 
 //fontion * 2 bouton multiplicateur, si assez de point pour le payer
 function incrementer() {
-    if (score >= 5) {
+    var prix = 5 * multi;
+    if (score >= prix) {
         multi = multi * 2;
-        score = score - 5;
+        score = score - prix;
     document.getElementById("multiplicateur").innerHTML = "Multiplicateur x" + multi;
     document.getElementById("prix").innerHTML= "Prix " + 5 * multi;
-    document.getElementById("score").innerHTML = score;
+    document.getElementById("score").innerHTML = "Points " + score;
     }
 
     else {
